887a0eb940a9666c33d864930f52cb517d61a6cc
chandranitu <chandranitu@gmail.com>
2021-11-24 08:46:23 +0530

    go files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-login
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+)
+
+func main() {
+	blog := New()
+	fmt.Println(blog)
+	blog.SaveArticle(Article{"My first Blog post", "Today, we will be talking about blogging"})
+	fmt.Println(blog)
+}
+
+//go run blog.go main.go
